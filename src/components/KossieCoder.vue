21ae6cebef0cf5d53644602a573a8e3a6d5fc3a1
jinkyungkim12 <skwlsrud3@naver.com>
2022-12-15 17:44:04 +0900

    up

---
+++ src/components/KossieCoder.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p>{{ name }}</p><br>
+        <button @click="updateName">Change Name</button>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return {
+            name: 'lemon-tree',
+        }
+    },
+    
+    methods: {
+        updateName(){
+            this.name = 'lemon-tree updated';
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
+  <!-- <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div> -->
+  <div>
+    <h1>This is Home page</h1>
+    <span>{{ name }}</span>
+    <KossieCoder></KossieCoder>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+// export default {
+//   name: 'HomeView',
+//   components: {
+//     HelloWorld
+//   }
+// }
+
+import KossieCoder from '@/components/KossieCoder.vue'
+
+  export default {
+    components: {
+      KossieCoder,
+    },
+    data(){
+      return {
+        name: '코지코더'
+      }
+    }    
   }
-}
+
 </script>
+
+<style scoped>
+  h1{
+    color: blue;
+  }
+</style>
